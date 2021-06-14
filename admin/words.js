0156a4f2824dc3942e76242d1aa6da3177f1d309
VBen <devel@velmeden.info>
2021-06-14 21:57:44 +0200

    INITIAL commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"motionblinds adapter settings": {
+		"en": "Adapter settings for motionblinds",
+		"de": "Adaptereinstellungen für motionblinds",
+		"ru": "Настройки адаптера для motionblinds",
+		"pt": "Configurações do adaptador para motionblinds",
+		"nl": "Adapterinstellingen voor motionblinds",
+		"fr": "Paramètres d'adaptateur pour motionblinds",
+		"it": "Impostazioni dell'adattatore per motionblinds",
+		"es": "Ajustes del adaptador para motionblinds",
+		"pl": "Ustawienia adaptera dla motionblinds",
+		"zh-cn": "motionblinds的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
---
+++ src/lib/adapter-config.d.ts
@@ -0,0 +1,13 @@
+// This file extends the AdapterConfig type from "@types/iobroker"
+
+// Augment the globally declared type ioBroker.AdapterConfig
+declare global {
+	namespace ioBroker {
+		interface AdapterConfig {
+			token: string;
+		}
+	}
+}
+
+// this is required so the above AdapterConfig is found by TypeScript / type checking
+export {};
