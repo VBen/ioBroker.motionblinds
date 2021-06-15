2fc8a0b5bc900c2a503d56d9f860df73bbfcfe10
Benjamin <devel@velmeden.info>
2021-06-15 23:19:15 +0200

    working proof of concept

--- build/main.js
+++ build/main.js
@@ -26,14 +26,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // you need to create an adapter
 const utils = __importStar(require("@iobroker/adapter-core"));
 const motionblinds_1 = require("motionblinds");
-// Load your modules here, e.g.:
-// import * as fs from "fs";
 class Motionblinds extends utils.Adapter {
     constructor(options = {}) {
         super({
             ...options,
             name: "motionblinds",
         });
+        this.devices = [];
+        this.devicemap = new Map();
         this.on("ready", this.onReady.bind(this));
         this.on("stateChange", this.onStateChange.bind(this));
         // this.on("objectChange", this.onObjectChange.bind(this));
@@ -45,36 +45,62 @@ class Motionblinds extends utils.Adapter {
      */
     async onReady() {
         // Initialize your adapter here
-        this.gateway = new motionblinds_1.MotionGateway();
-        this.gateway.token = this.config.token;
         // The adapters config (in the instance object everything under the attribute "native") is accessible via
         // this.config:
         this.log.info("config token: " + this.config.token);
-        /*
-                this.gateway.on('report', (report: string) => {
-                    this.log.debug(report)
-                  })
-        */
         // Reset the connection indicator during startup
         this.setState("info.connection", false, true);
+        this.gateway = new motionblinds_1.MotionGateway({ key: this.config.token });
+        this.gateway.start();
+        this.gateway.on("report", (report) => {
+            this.updateFromReport(report);
+        });
+        this.gateway.on("error", (err) => {
+            this.log.error("Error: " + JSON.stringify(err));
+        });
+        this.gateway.on("heartbeat", (heartbeat) => {
+            this.log.debug("Heartbeat: " + JSON.stringify(heartbeat));
+        });
+        // Reset the connection indicator during startup
+        this.setState("info.connection", false, true);
+        this.log.info("Fetching device list");
+        this.devices = await this.gateway.readAllDevices()
+            .catch((reason) => this.log.error("Failed fetching list of MOTION Blinds: " + JSON.stringify(reason)));
+        if (this.devices) {
+            this.setState("info.connection", true, true);
+            this.log.info("Devices: " + JSON.stringify(this.devices));
+            for (const dev of this.devices) {
+                this.devicemap.set(dev.mac, { devtype: dev.deviceType });
+            }
+        }
+        for (const [mac, data] of this.devicemap) {
+            await this.gateway.readDevice(mac, data.devtype)
+                .then((value) => {
+                const reportdata = { msgType: "Report", data: value.data, mac: mac, deviceType: data.devtype };
+                this.updateFromReport(reportdata);
+            })
+                .catch((err) => { return err; });
+        }
+        this.log.debug(JSON.stringify(this.devices));
+        this.subscribeStates("*.position");
+        this.subscribeStates("*.fullup");
+        this.subscribeStates("*.fulldown");
+        this.subscribeStates("*.stop");
+        this.subscribeStates("*.device_query");
+        this.subscribeStates("*.angle");
         /*
-        For every state in the system there has to be also an object of type state
-        Here a simple template for a boolean variable named "testVariable"
-        Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-        */
-        await this.setObjectNotExistsAsync("testVariable", {
-            type: "state",
-            common: {
-                name: "testVariable",
+        For every state in the system there has to be alDeviceType
                 type: "boolean",
                 role: "indicator",
                 read: true,
                 write: true,
             },
             native: {},
-        });
+        });*/
+        /*const states = this.getStatesAsync("*.position");
+        this.log.silly("states:" + JSON.stringify(states));*/
         // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-        this.subscribeStates("testVariable");
+        // this.subscribeStates("testVariable");
         // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
         // this.subscribeStates("lights.*");
         // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
@@ -84,24 +110,23 @@ class Motionblinds extends utils.Adapter {
             you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
         */
         // the variable testVariable is set to true as command (ack=false)
-        await this.setStateAsync("testVariable", true);
-        // same thing, but the value is flagged "ack"
-        // ack should be always set to true if the value is received from or acknowledged from the target system
-        await this.setStateAsync("testVariable", { val: true, ack: true });
-        // same thing, but the state is deleted after 30s (getState will return null afterwards)
-        await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
-        // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync("admin", "iobroker");
-        this.log.info("check user admin pw iobroker: " + result);
-        result = await this.checkGroupAsync("admin", "admin");
-        this.log.info("check group user admin group admin: " + result);
+        /*await this.setStateAsync("testVariable", true);
+
+        // same thing, but the vaimport  * as os from "os";p user admin group admin: " + result);*/
     }
     /**
      * Is called when adapter shuts down - callback has to be called under any circumstances!
      */
     onUnload(callback) {
+        this.log.info("Shutting down adapter");
         try {
-            this.gateway.stop;
+            if (this.gateway) {
+                if (this.gateway.sendSocket)
+                    this.gateway.sendSocket.close();
+                if (this.gateway.recvSocket)
+                    this.gateway.recvSocket.close();
+                this.gateway.stop();
+            }
             // Here you must clear all timeouts or intervals that may still be active
             // clearTimeout(timeout1);
             // clearTimeout(timeout2);
@@ -130,14 +155,153 @@ class Motionblinds extends utils.Adapter {
     /**
      * Is called if a subscribed state changes
      */
-    onStateChange(id, state) {
+    async onStateChange(id, state) {
+        var _a, _b, _c, _d, _e, _f, _g;
         if (state) {
             // The state was changed
             this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+            let doUpdate = false;
+            const devicetype = (_a = this.devicemap.get(this.getMacForID(id))) === null || _a === void 0 ? void 0 : _a.devtype;
+            if (devicetype) {
+                if (state.ack == false && id.search("position") > 0) {
+                    await ((_b = this.gateway) === null || _b === void 0 ? void 0 : _b.writeDevice(this.getMacForID(id), devicetype, { targetPosition: Number(state.val) }).then((value) => { this.log.info("got ack: " + JSON.stringify(value)); }).catch((err) => { this.log.error("got error while writing: " + JSON.stringify(err)); }));
+                    doUpdate = true;
+                }
+                else if (state.ack == false && id.search("angle") > 0) {
+                    await ((_c = this.gateway) === null || _c === void 0 ? void 0 : _c.writeDevice(this.getMacForID(id), devicetype, { targetAngle: Number(state.val) }).then((value) => { this.log.info("got ack: " + JSON.stringify(value)); }).catch((err) => { this.log.error("got error while writing: " + JSON.stringify(err)); }));
+                    doUpdate = true;
+                }
+                else if (state.ack == false && id.search("fullup") > 0) {
+                    await ((_d = this.gateway) === null || _d === void 0 ? void 0 : _d.writeDevice(this.getMacForID(id), devicetype, { operation: 1 }).then((value) => { this.log.info("got ack: " + JSON.stringify(value)); }).catch((err) => { this.log.error("got error while writing: " + JSON.stringify(err)); }));
+                }
+                else if (state.ack == false && id.search("fulldown") > 0) {
+                    await ((_e = this.gateway) === null || _e === void 0 ? void 0 : _e.writeDevice(this.getMacForID(id), devicetype, { operation: 0 }).then((value) => { this.log.info("got ack: " + JSON.stringify(value)); }).catch((err) => { this.log.error("got error while writing: " + JSON.stringify(err)); }));
+                }
+                else if (state.ack == false && id.search("stop") > 0) {
+                    await ((_f = this.gateway) === null || _f === void 0 ? void 0 : _f.writeDevice(this.getMacForID(id), devicetype, { operation: 2 }).then((value) => { this.log.info("got ack: " + JSON.stringify(value)); }).catch((err) => { this.log.error("got error while writing: " + JSON.stringify(err)); }));
+                }
+                if (state.ack == false && doUpdate) {
+                    await ((_g = this.gateway) === null || _g === void 0 ? void 0 : _g.writeDevice(this.getMacForID(id), devicetype, { operation: 5 }).then((value) => { this.log.info("got ack: " + JSON.stringify(value)); }).catch((err) => { this.log.error("got error while writing: " + JSON.stringify(err)); }));
+                }
+            }
         }
         else {
             // The state was deleted
             this.log.info(`state ${id} deleted`);
+        }
+    }
+    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
+    // /**
+    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
+    //  */
+    // private onMessage(obj: ioBroker.Message): void {
+    // 	if (typeof obj === "object" && obj.message) {
+    // 		if (obj.command === "send") {
+    // 			// e.g. send email or pushover or whatever
+    // 			this.log.info("send command");
+    // 			// Send response in callback if required
+    // 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
+    // 		}
+    // 	}
+    // }
+    getMacForID(id) {
+        const splitted_id = id.split(".");
+        return splitted_id[splitted_id.length - 2];
+    }
+    updateFromReport(report) {
+        this.log.debug("Report: " + JSON.stringify(report));
+        this.setObjectNotExists(report.mac, {
+            type: "channel",
+            common: {
+                name: report.mac,
+                role: "blind"
+            },
+            native: {
+                mac: report.mac,
+                deviceType: report.deviceType
+            }
+        });
+        const data = report.data;
+        Object.keys(data).forEach((key, idx) => {
+            let value = Object.values(data)[idx];
+            let dp = report.mac + "." + key;
+            let name = key;
+            let unit = "";
+            let type = "string";
+            let write = false;
+            switch (key) {
+                case "type":
+                    name = "Blind type";
+                    value = motionblinds_1.BlindType[value].toString();
+                    break;
+                case "operation":
+                    name = "Current operation";
+                    value = motionblinds_1.Operation[value].toString();
+                    break;
+                case "currentAngle":
+                    dp = dp = report.mac + ".angle";
+                    name = "Shutter Angle";
+                    type = "number";
+                    unit = "°";
+                    write = true;
+                    break;
+                case "voltageMode":
+                    name = "Motor type";
+                    value = motionblinds_1.VoltageMode[value].toString();
+                    break;
+                case "currentState":
+                    name = "Current state";
+                    value = motionblinds_1.LimitsState[value].toString();
+                    break;
+                case "wirelessMode":
+                    name = "Wireless type";
+                    value = motionblinds_1.WirelessMode[value].toString();
+                    break;
+                case "RSSI":
+                    type = "number";
+                    break;
+                case "currentPosition":
+                    dp = dp = report.mac + ".position";
+                    name = "Position";
+                    type = "number";
+                    write = true;
+                    unit = "%";
+                    break;
+                case "batteryLevel":
+                    name = "Battery Level";
+                    unit = "%";
+                    type = "number";
+                    value = value / 10;
+                    break;
+                default:
+            }
+            this.setObjectNotExists(dp, {
+                type: "state",
+                common: {
+                    name: name,
+                    role: "blind",
+                    type: type,
+                    read: true,
+                    write: write,
+                    unit: unit
+                },
+                native: {}
+            });
+            this.setState(dp, value, true);
+        });
+        const btns = ["fullup", "fulldown", "stop", "device_query"];
+        for (const btn of btns) {
+            this.setObjectNotExists(report.mac + "." + btn, {
+                type: "state",
+                common: {
+                    name: btn,
+                    role: "button",
+                    type: "boolean",
+                    read: false,
+                    write: true,
+                }, native: {}
+            });
         }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,22 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 import * as utils from "@iobroker/adapter-core";
-import { MotionGateway } from "motionblinds";
+import { MotionGateway, Report, BlindType, ReadDeviceAck, DeviceType, Operation, VoltageMode, LimitsState, WirelessMode } from "motionblinds";
 
 
 // Load your modules here, e.g.:
 // import * as fs from "fs";
-
+type DeviceData = {
+	devtype: DeviceType
+}
 
 class Motionblinds extends utils.Adapter {
-	private gateway?: any;
-	private devices?: any;
+
+	private gateway?: MotionGateway;
+	private devices: void | ReadDeviceAck[] = [];
+	private devicemap = new Map<string, DeviceData>();
+
+
 	public constructor(options: Partial<utils.AdapterOptions> = {}) {
 		super({
 			...options,
@@ -32,43 +38,70 @@ class Motionblinds extends utils.Adapter {
 	 */
 	private async onReady(): Promise<void> {
 		// Initialize your adapter here
-		
-		this.gateway = new MotionGateway();
-		this.gateway.token = this.config.token;
 		// The adapters config (in the instance object everything under the attribute "native") is accessible via
 		// this.config:
 		this.log.info("config token: " + this.config.token);
-/*
-		this.gateway.on('report', (report: string) => {
-			this.log.debug(report)
-		  })
-*/		
+		// Reset the connection indicator during startup
+		this.setState("info.connection", false, true);
+		this.gateway = new MotionGateway({ key: this.config.token });
+
+		this.gateway.start();
+
+		this.gateway.on("report", (report) => {
+			this.updateFromReport(report);
+		})
+		this.gateway.on("error", (err) => {
+			this.log.error("Error: " + JSON.stringify(err));
+		})
+		this.gateway.on("heartbeat", (heartbeat) => {
+			this.log.debug("Heartbeat: " + JSON.stringify(heartbeat));
+		})
 
 		// Reset the connection indicator during startup
 		this.setState("info.connection", false, true);
+		this.log.info("Fetching device list");
+		this.devices = await this.gateway.readAllDevices()
+			.catch((reason) => this.log.error("Failed fetching list of MOTION Blinds: " + JSON.stringify(reason)));
+		if (this.devices) {
+			this.setState("info.connection", true, true);
+			this.log.info("Devices: " + JSON.stringify(this.devices));
+			for (const dev of this.devices) {
+				this.devicemap.set(dev.mac, { devtype: dev.deviceType });
+			}
 
+		}
+		for (const [mac, data] of this.devicemap) {
 
+			await this.gateway.readDevice(mac, data.devtype)
+				.then((value) => {
+					const reportdata = { msgType: "Report", data: value.data, mac: mac, deviceType: data.devtype } as Report;
+					this.updateFromReport(reportdata);
+				})
+				.catch((err) => { return err }
+				)
+		}
+		this.log.debug(JSON.stringify(this.devices));
 
+		this.subscribeStates("*.position");
+		this.subscribeStates("*.fullup");
+		this.subscribeStates("*.fulldown");
+		this.subscribeStates("*.stop");
+		this.subscribeStates("*.device_query");
+		this.subscribeStates("*.angle");
 
 		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectNotExistsAsync("testVariable", {
-			type: "state",
-			common: {
-				name: "testVariable",
+		For every state in the system there has to be alDeviceType
 				type: "boolean",
 				role: "indicator",
 				read: true,
 				write: true,
 			},
 			native: {},
-		});
-
+		});*/
+		/*const states = this.getStatesAsync("*.position");
+		this.log.silly("states:" + JSON.stringify(states));*/
 		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates("testVariable");
+		// this.subscribeStates("testVariable");
 		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
 		// this.subscribeStates("lights.*");
 		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
@@ -79,29 +112,22 @@ class Motionblinds extends utils.Adapter {
 			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
 		*/
 		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
+		/*await this.setStateAsync("testVariable", true);
 
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync("testVariable", { val: true, ack: true });
-
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
-
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
-		this.log.info("check user admin pw iobroker: " + result);
-
-		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
+		// same thing, but the vaimport  * as os from "os";p user admin group admin: " + result);*/
 	}
 
 	/**
 	 * Is called when adapter shuts down - callback has to be called under any circumstances!
 	 */
 	private onUnload(callback: () => void): void {
+		this.log.info("Shutting down adapter");
 		try {
-			this.gateway.stop;
+			if (this.gateway) {
+				if (this.gateway.sendSocket) this.gateway.sendSocket.close();
+				if (this.gateway.recvSocket) this.gateway.recvSocket.close();
+				this.gateway.stop();
+			}
 			// Here you must clear all timeouts or intervals that may still be active
 			// clearTimeout(timeout1);
 			// clearTimeout(timeout2);
@@ -113,6 +139,7 @@ class Motionblinds extends utils.Adapter {
 			callback();
 		}
 	}
+
 
 	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
 	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
@@ -132,10 +159,46 @@ class Motionblinds extends utils.Adapter {
 	/**
 	 * Is called if a subscribed state changes
 	 */
-	private onStateChange(id: string, state: ioBroker.State | null | undefined): void {
+	private async onStateChange(id: string, state: ioBroker.State | null | undefined): Promise<void> {
+
 		if (state) {
 			// The state was changed
 			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+			let doUpdate = false;
+			const devicetype = this.devicemap.get(this.getMacForID(id))?.devtype;
+			if (devicetype) {
+				if (state.ack == false && id.search("position") > 0) {
+					await this.gateway?.writeDevice(this.getMacForID(id), devicetype, { targetPosition: Number(state.val) })
+						.then((value) => { this.log.info("got ack: " + JSON.stringify(value)) })
+						.catch((err) => { this.log.error("got error while writing: " + JSON.stringify(err)) });
+					doUpdate = true;
+
+				} else if (state.ack == false && id.search("angle") > 0) {
+					await this.gateway?.writeDevice(this.getMacForID(id), devicetype, { targetAngle: Number(state.val) })
+						.then((value) => { this.log.info("got ack: " + JSON.stringify(value)) })
+						.catch((err) => { this.log.error("got error while writing: " + JSON.stringify(err)) });
+					doUpdate = true;
+
+				} else if (state.ack == false && id.search("fullup") > 0) {
+					await this.gateway?.writeDevice(this.getMacForID(id), devicetype, { operation: 1 })
+						.then((value) => { this.log.info("got ack: " + JSON.stringify(value)) })
+						.catch((err) => { this.log.error("got error while writing: " + JSON.stringify(err)) });
+
+				} else if (state.ack == false && id.search("fulldown") > 0) {
+					await this.gateway?.writeDevice(this.getMacForID(id), devicetype, { operation: 0 })
+						.then((value) => { this.log.info("got ack: " + JSON.stringify(value)) })
+						.catch((err) => { this.log.error("got error while writing: " + JSON.stringify(err)) });
+				} else if (state.ack == false && id.search("stop") > 0) {
+					await this.gateway?.writeDevice(this.getMacForID(id), devicetype, { operation: 2 })
+						.then((value) => { this.log.info("got ack: " + JSON.stringify(value)) })
+						.catch((err) => { this.log.error("got error while writing: " + JSON.stringify(err)) });
+				}
+				if (state.ack == false && doUpdate) {
+					await this.gateway?.writeDevice(this.getMacForID(id), devicetype, { operation: 5 })
+						.then((value) => { this.log.info("got ack: " + JSON.stringify(value)) })
+						.catch((err) => { this.log.error("got error while writing: " + JSON.stringify(err)) });
+				}
+			}
 		} else {
 			// The state was deleted
 			this.log.info(`state ${id} deleted`);
@@ -158,9 +221,114 @@ class Motionblinds extends utils.Adapter {
 	// 		}
 	// 	}
 	// }
+	private getMacForID(id: string): string {
+		const splitted_id: string[] = id.split(".");
+		return splitted_id[splitted_id.length - 2];
+	}
 
+	private updateFromReport(report: Report): void {
+		this.log.debug("Report: " + JSON.stringify(report));
+		this.setObjectNotExists(report.mac, {
+			type: "channel",
+			common: {
+				name: report.mac,
+				role: "blind"
+			},
+			native: {
+				mac: report.mac,
+				deviceType: report.deviceType
+			}
+		});
+		const data = report.data;
+		Object.keys(data).forEach((key, idx) => {
+			let value: any = Object.values(data)[idx];
+
+			let dp = report.mac + "." + key;
+			let name: string = key;
+			let unit = "";
+			let type: any = "string";
+			let write = false;
+
+			switch (key) {
+				case "type":
+					name = "Blind type";
+					value = BlindType[value].toString();
+					break;
+				case "operation":
+					name = "Current operation";
+					value = Operation[value].toString();
+					break;
+				case "currentAngle":
+					dp = dp = report.mac + ".angle";
+					name = "Shutter Angle";
+					type = "number";
+					unit = "°"
+					write = true;
+					break;
+				case "voltageMode":
+					name = "Motor type"
+					value = VoltageMode[value].toString();
+					break;
+				case "currentState":
+					name = "Current state";
+					value = LimitsState[value].toString();
+					break;
+				case "wirelessMode":
+					name = "Wireless type";
+					value = WirelessMode[value].toString();
+					break;
+				case "RSSI":
+					type = "number";
+					break;
+				case "currentPosition":
+					dp = dp = report.mac + ".position";
+					name = "Position";
+					type = "number";
+					write = true;
+					unit = "%";
+					break;
+				case "batteryLevel":
+					name = "Battery Level";
+					unit = "%";
+					type = "number"
+					value = value / 10;
+					break;
+
+				default:
+			}
+
+
+			this.setObjectNotExists(dp, {
+				type: "state",
+				common: {
+					name: name,
+					role: "blind",
+					type: type,
+					read: true,
+					write: write,
+					unit: unit
+				},
+				native: {}
+
+			});
+			this.setState(dp, value, true);
+		});
+		const btns = ["fullup", "fulldown", "stop", "device_query"];
+		for (const btn of btns) {
+			this.setObjectNotExists(report.mac + "." + btn, {
+				type: "state",
+				common: {
+					name: btn,
+					role: "button",
+					type: "boolean",
+					read: false,
+					write: true,
+				}, native: {}
+			});
+
+		}
+	}
 }
-
 if (require.main !== module) {
 	// Export the constructor in compact mode
 	module.exports = (options: Partial<utils.AdapterOptions> | undefined) => new Motionblinds(options);
